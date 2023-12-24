b8e64cf4fc2f679a6a7458e9a010fa5a48741ba3
BratsunDanila <hardy444@mail.ru>
2023-12-24 23:51:30 +0500

    ОПА F5

--- Tyuiu.BratsunDA.Sprint7.Project.V12.Test/DataServiceTest.cs
+++ Tyuiu.BratsunDA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
+using Tyuiu.BratsunDA.Sprint7.Project.V12.Lib;
 
 namespace Tyuiu.BratsunDA.Sprint7.Project.V12.Test
 {
@@ -7,8 +9,14 @@ namespace Tyuiu.BratsunDA.Sprint7.Project.V12.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetBase()
         {
+            DataService ds = new DataService();
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\TEST.csv";
+            string[,] res = ds.GetBase(pathSaveFile);
+            string[,] wait = { { "bebra11", "bebra21" }, { "bebra12", "bebra22" }, { "ZACHEM YA", "ETO DELAYU" } };
+
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
